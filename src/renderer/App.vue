24420003924f6d89edfd08c5c97af7232b1688fe
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2018-05-21 22:35:06 +0200

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        Hello World!
+    </div>
+</template>
+
+<script>
+    import './styles/app.sass'
+
+    export default {
+        //
+    }
+</script>
+
+<!-- <style src="./styles/app.sass"></style> -->
